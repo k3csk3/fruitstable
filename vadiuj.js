6f477eafede90de691037ac2553a8751cdeffd65
k3csk3 <szilagyi.viktor89@gmail.com>
2022-06-03 09:41:44 +0200

    upgrade

--- vadiuj.js
+++ vadiuj.js
@@ -13,7 +13,7 @@ for (let i = 0; i < fruits.length; i++) {
     //console.log(fruits[i])
     //let row = `<tr><td>` + fruits[i] + `</td><td><button type="button" class="btn btn-sm btn-danger" >Delete</button></td> </tr>`
     //let row = `<tr><td>${fruits[i]}</td><td><button type="button" class="btn btn-sm btn-danger" >Delete</button></td> </tr>`
-    rows += `<tr><td>${fruits[i]}</td><td><button type="button" class="btn btn-sm btn-danger" onclick="btnDeleteRow(this)">Delete</button></td> </tr>`
+    rows += `<tr><td>${fruits[i]}</td><td><button type="button" class="btn btn-sm btn-danger" onclick="btnDeleteRow(this)">Delete</button><button type="button" class="btn btn-sm btn-primary" onclick="btnChangeRow(this)">Change</button></td> </tr>`
 }
 
 document.querySelector("tbody").innerHTML = rows
@@ -29,4 +29,14 @@ function btnDeleteRow(btnElement) {
 function btnAddFruit() {
     let newFruit = document.querySelector("input[name='newFruit']")
     document.querySelector("tbody").innerHTML += `<tr><td>${newFruit.value}</td><td><button type="button" class="btn btn-sm btn-danger" onclick="btnDeleteRow(this)">Delete</button></td> </tr>`
+}
+
+// Gyümölcscsere
+
+function btnChangeRow(btnChange) {
+    let addFruit = prompt("Change fruit", "")
+    if (addFruit !=null) {
+        btnChange.closest('tr').remove();
+        document.querySelector("tbody").innerHTML += `<tr><td>${addFruit}</td><td><button type="button" class="btn btn-sm btn-danger" onclick="btnDeleteRow(this)">Delete</button><button type="button" class="btn btn-sm btn-primary" onclick="btnChangeRow(this)">Change</button></td> </tr>`
+    }
 }
